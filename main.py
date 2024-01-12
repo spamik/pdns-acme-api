1e8347b7da2a57a8d45554f102882b8327789fbd
spm <jan.krajdl@cecolo.com>
2024-01-12 17:32:42 +0100

    Added pDNS views

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import requests
 
 app = FastAPI()
+PDNS_URL = 'http://172.16.14.28:8081'
+PDNS_ZONE_URL = '/api/v1/servers/localhost/zones'
+HEADERS = {
+    'X-API-Key': '0123456789ABCDEF'
+}
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get(PDNS_ZONE_URL)
+async def get_zones():
+    response = requests.get(PDNS_URL + PDNS_ZONE_URL, headers=HEADERS)
+    return response.content
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.patch(PDNS_ZONE_URL + '/{zone}')
+async def patch_zone(request: Request, zone: str):
+    response = requests.patch(PDNS_ZONE_URL + '/{zone}', headers=HEADERS, data=request.json())
+    return response.content
