e694ef885d81f247372405fec7e9c8d0cee02864
spm <jan.krajdl@cecolo.com>
2024-01-15 16:24:56 +0100

    JSON response fix
    - return pdns response as json, not string with double quotes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Request
 import requests
+import json
 
 app = FastAPI()
-PDNS_URL = 'http://172.16.14.28:8081'
+PDNS_URL = 'http://172.16.14.28:8082'
 PDNS_ZONE_URL = '/api/v1/servers/localhost/zones'
 HEADERS = {
     'X-API-Key': '0123456789ABCDEF'
@@ -12,10 +13,10 @@ HEADERS = {
 @app.get(PDNS_ZONE_URL)
 async def get_zones():
     response = requests.get(PDNS_URL + PDNS_ZONE_URL, headers=HEADERS)
-    return response.content
+    return json.loads(response.content)
 
 
 @app.patch(PDNS_ZONE_URL + '/{zone}')
 async def patch_zone(request: Request, zone: str):
     response = requests.patch(PDNS_ZONE_URL + '/{zone}', headers=HEADERS, data=request.json())
-    return response.content
+    return json.loads(response.content)
